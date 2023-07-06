774c7fbd19f8ea2f5a90409b4c30b5c6bfe495b4
Mehdi.M <mehdi@Mehdis-MacBook-Pro.local>
2023-07-06 18:57:05 +0000

    Add relation user to agent

--- apps/agent/models.py
+++ apps/agent/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from utils.fakers import fake_name
 
+User = get_user_model()
+
 
 class Agent(models.Model):
     name = models.CharField(max_length=100, default=fake_name)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
