4b816ebbb3fc01a5139bcc58a6f2c4955b8161c5
Mehdi.M <mehdi@Mehdis-MacBook-Pro.local>
2023-07-07 15:23:18 +0000

    Add module to use generate fake data

--- utils/fakers.py
+++ utils/fakers.py
@@ -1,7 +1,14 @@
-from faker import Faker
+import random
+from datetime import timedelta
 
+from faker import Faker
+from django.utils import timezone
 fake = Faker()
 
 
-def fake_name():
+def fake_name() -> str:
     return fake.name()
+
+
+def fake_delivery_time() -> int:
+    return timezone.now() + timedelta(minutes=random.randint(0, 10))
